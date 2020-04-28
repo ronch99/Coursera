391cc0bb9241515ecc657fc935afde05aa59c34d
Ziru "Ron" Chen <47461841+ronch99@users.noreply.github.com>
2020-04-27 22:00:56 -0400

    Update costFunctionReg.m

--- MLAndrewNg/LogisticRegression/costFunctionReg.m
+++ MLAndrewNg/LogisticRegression/costFunctionReg.m
@@ -8,23 +8,11 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 n = length(theta);
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-
 J = (-1 / m) * (y' * log(sigmoid(X * theta))...
     + (1 - y)' * log((1 - sigmoid(X * theta))))...
     + (lambda / (2 * m)) * (theta(2:n)' * theta(2:n));
 
 grad = (1 / m) * (X' * (sigmoid(X * theta) - y))...
     + [0; (lambda / m) * theta(2:n)];
-
-% =============================================================
 
 end
